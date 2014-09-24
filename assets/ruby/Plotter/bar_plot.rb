c5b509750e33d5090f21d39f525df01dcdbb6dec
KC Erb <iamkcerb@gmail.com>
2014-09-23 22:37:27 -0600

    added links and reran all code for sanity

--- assets/ruby/Plotter/bar_plot.rb
+++ assets/ruby/Plotter/bar_plot.rb
@@ -1,19 +1,20 @@
 module Plotter
-class BarPlot < BasePlot
-  def generate_marks
-    plot = self
-    @marks = rect_mark.from(@data) do
-      enter do
-        x_start   { scale(plot.x_scale).from('x') }
-        width     { scale(plot.x_scale).value(0.9)}
-        y_start   { scale(plot.y_scale).from('y') }
-        y_end     { scale(plot.y_scale).value(0) }
-      end
-      update do
-        fill plot.opts[:fill] || 'steelblue'
-      end
-      hover do
-        fill 'limegreen'
+  class BarPlot < BasePlot
+    def generate_marks
+      plot = self
+      @marks = rect_mark.from(@data) do
+        enter do
+          x_start   { scale(plot.x_scale).from('x') }
+          width     { scale(plot.x_scale).value(0.9)}
+          y_start   { scale(plot.y_scale).from('y') }
+          y_end     { scale(plot.y_scale).value(0) }
+        end
+        update do
+          fill plot.opts[:fill] || 'steelblue'
+        end
+        hover do
+          fill 'limegreen'
+        end
       end
     end
   end
